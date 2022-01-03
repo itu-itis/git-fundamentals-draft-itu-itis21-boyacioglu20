062e531b37e526686fbcc8ef97b3f95c93fb0c17
itu-itis21-boyacioglu20 <boyacioglu20@itu.edu.tr>
2022-01-03 11:04:51 +0300

    Form added

---
+++ main.js
@@ -0,0 +1,9 @@
+var insideUL = document.getElementById("insideUL")
+
+function onMouseOver() {
+    insideUL.style.backgroundColor = "purple";
+}
+
+function onClick() {
+    insideUL.style.backgroundColor = "orange";
+}
